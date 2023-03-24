d4bffda4927c243eb371c52a0b966974e56fe951
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-24 16:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const promises = [];
+for (let i = 1; i <= 5; i++) {
+  const promise = new Promise((resolve, reject) => {
+    const randomNumber = getRandomNumber(1, 10);
+    const shouldReject = Math.random() < 0.5;
+
+    if (shouldReject) {
+      reject(`Promise ${i} rejected with error`);
+    } else {
+      resolve(randomNumber);
+    }
+  });
+  promises.push(promise);
+}
+
+Promise.all(promises)
+  .then((results) => {
+    for (let i = 0; i < results.length; i++) {
+      output.innerHTML += `<p>Promise ${i + 1}: ${results[i]}</p>`;
+    }
+  })
+  .catch((error) => {
+    for (let i = 0; i < promises.length; i++) {
+      output.innerHTML += `<p>${error}</p>`;
+    }
+  });
